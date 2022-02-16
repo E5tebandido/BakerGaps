e87ffdc46cd0503abcedf95a0f7120b36343b6c6
Teban18 <aepr_18_@hotmail.com>
2022-02-16 02:46:28 -0500

    index styling

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express();
+const path = require('path');
 
 app.use(express.static(__dirname));
 
 app.get('/taccounts', (req, res) => {
-    res.send('hellooo')
+    res.sendFile('taccounts.html', {
+        root: path.join(__dirname, 'views/')
+    });
+})
+
+app.get('/results', (req, res) => {
+    res.sendFile('results.html', {
+        root: path.join(__dirname, 'views/')
+    });
+})
+
+app.get('/heritage', (req, res) => {
+    res.sendFile('heritage.html', {
+        root: path.join(__dirname, 'views/')
+    });
 })
 
 app.listen('8080');
